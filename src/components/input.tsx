685e8b979f12d83bfe294ac1a24ac2c7da25b87d
Sebastian Zapata <sebastian.zapata@travelchapter.com>
2020-12-04 15:07:16 +0000

    change form to div

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -3,9 +3,14 @@ import React, { useState } from 'react';
 const Input: React.FunctionComponent = () => {
   const [softnessValue, setSoftnessValue] = useState<number>(undefined);
 
+  const sendData = () => {
+    console.log("softness", softnessValue)
+  }
+
   return (
-    <form onSubmit={() => console.log(softnessValue)}>
+    <div>
       <select name="stoolSoftness" onChange={(e) => setSoftnessValue(parseInt(e.target.value, 10))}>
+        <option value={undefined}>--Please select a value--</option>
         <option value={1}>1 - Severe constipation</option>
         <option value={2}>2 - Mild constipation</option>
         <option value={3}>3 - Normal</option>
@@ -14,8 +19,8 @@ const Input: React.FunctionComponent = () => {
         <option value={6}>6 - Mild diarrhoea</option>
         <option value={7}>7 - Severe diarrhoea</option>
       </select>
-      <input type="submit" value="Submit" />
-    </form>
+      <input type="submit" value="Submit" onClick={sendData} />
+    </div>
   )
 };
 
